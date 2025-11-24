6a9ae8d91808800967212d3c8ee2a3cce6ab5f4f
Joao Pedro Alves <joao163001@gmail.com>
2025-11-24 20:17:41 -0300

    Implementação da funcionalidade de Listagem de produtos no ProdutosDAO e preenchimento do JTable na listagemVIEW.

--- src/ProdutosDAO.java
+++ src/ProdutosDAO.java
@@ -46,8 +46,45 @@ public class ProdutosDAO {
     }
 
     public ArrayList<ProdutosDTO> listarProdutos() {
+        String sql = "SELECT id, nome, valor, status FROM produtos";
+        conn = new conectaDAO().connectDB();
 
-        return listagem;
+        try {
+            prep = conn.prepareStatement(sql);
+            resultset = prep.executeQuery();
+
+            ArrayList<ProdutosDTO> listagem = new ArrayList<>();
+
+            while (resultset.next()) {
+                ProdutosDTO produto = new ProdutosDTO();
+                produto.setId(resultset.getInt("id"));
+                produto.setNome(resultset.getString("nome"));
+                produto.setValor(resultset.getInt("valor"));
+                produto.setStatus(resultset.getString("status"));
+
+                listagem.add(produto);
+            }
+
+            return listagem;
+
+        } catch (SQLException erro) {
+            JOptionPane.showMessageDialog(null, "Erro ao listar produtos: " + erro.getMessage());
+            return new ArrayList<>();
+        } finally {
+            try {
+                if (resultset != null) {
+                    resultset.close();
+                }
+                if (prep != null) {
+                    prep.close();
+                }
+                if (conn != null) {
+                    conn.close();
+                }
+            } catch (SQLException e) {
+                // Tratamento de erro ao fechar
+            }
+
+        }
     }
-
 }
